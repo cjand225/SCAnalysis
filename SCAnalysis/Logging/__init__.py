4d40ad5c0ce962cc5994d3f654f936c96b7516a0
Charles <cjan225@g.uky.edu>
2019-02-19 19:52:38 -0500

    fixed logging paths

--- SCAnalysis/Logging/__init__.py
+++ SCAnalysis/Logging/__init__.py
@@ -1,3 +1,7 @@
 import os, logging
 
+pkgName = "SCAnalysis"
+logFile = pkgName + ".log"
+logDir = os.path.abspath(os.path.join(__file__, "./../../Settings/Logs"))
+logPath = os.path.abspath(os.path.join(logDir, logFile))
 logDefaultFormat = logging.Formatter('[%(asctime)s] - [%(name)s] - [%(levelname)s] - %(message)s')
--- SCAnalysis/__init__.py
+++ SCAnalysis/__init__.py
@@ -1,13 +1,10 @@
+from SCAnalysis.Logging import logPath, logDir
 from SCAnalysis.Logging.Log import initLog
 from SCAnalysis.System.FileSystem import dirExists, makeDir
 
 import logging, os
 
 pkgName = "SCAnalysis"
-logFile = pkgName + ".log"
-
-logDir = os.path.abspath(os.path.join(__file__, "./../../Settings/Logs"))
-logPath = os.path.abspath(os.path.join(logDir, logFile))
 
 # check if dir exists, if not create it
 if not dirExists(logPath):
