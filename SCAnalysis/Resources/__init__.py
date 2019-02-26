0cef11f65dd8c8da83407d0697e0c71a21b860cf
Charles <cjan225@g.uky.edu>
2019-02-25 19:39:58 -0500

    Added resources and updated pathing

--- SCAnalysis/Resources/__init__.py
+++ SCAnalysis/Resources/__init__.py
@@ -1,9 +1,17 @@
 import os, sys
 
 # UI file Dir
-resourceDir = os.path.abspath(os.path.join(__file__, "../UI"))
+resourceDir = os.path.abspath(os.path.join(__file__, "."))
+uiDir = os.path.abspath(os.path.join(resourceDir, "../UI"))
+docDir = os.path.abspath(os.path.join(resourceDir, "../Docs"))
 
-mainUIPath = os.path.join(resourceDir, 'MainWindow.ui')
-logUIPath = os.path.join(resourceDir, 'LogWidget.ui')
-quitDialogUIPath = os.path.join(resourceDir, 'QuitDialog.ui')
-helpDialogUIPath = os.path.join(resourceDir, 'HelpDialog.ui')
+mainUIPath = os.path.join(uiDir, 'MainWindow.ui')
+logUIPath = os.path.join(uiDir, 'LogWidget.ui')
+quitDialogUIPath = os.path.join(uiDir, 'QuitDialog.ui')
+helpDialogUIPath = os.path.join(uiDir, 'HelpDialog.ui')
+
+# Documentation reference paths
+
+aboutDocPath = os.path.join(docDir, "About.md")
+adminDocPath = os.path.join(docDir, "AdminManual.md")
+userDocPath = os.path.join(docDir, "UserManual.md")
